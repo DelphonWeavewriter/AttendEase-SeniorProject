52040f2381070083e71d9ec05cd01a4af13b28b6
steven <r3dpotpocket@yahoo.com>
2025-11-20 15:15:04 -0500

    Main classes for socialease including setters, getters, and variables

---
+++ app/src/main/java/com/example/attendeasecampuscompanion/MessagingFragment.kt
@@ -0,0 +1,29 @@
+package com.example.attendeasecampuscompanion
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class MessagingFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_messaging, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.backButton).setOnClickListener {
+            requireActivity().finish()
+        }
+
+        view.findViewById<TextView>(R.id.messagingTitle).text = "Messages - Coming Soon"
+    }
+}
